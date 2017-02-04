1fd1c343f1aa501b9efe5e3ac145a8675ba3235a
SerLonic <sergeyfedorenko1997@gmail.com>
2017-02-04 21:16:16 +0200

    Ternary

---
+++ JavaProj/JavaApp4/src/JavaApp4.java
@@ -0,0 +1,12 @@
+/**
+ * Created by SerLonic on 04.02.2017.
+ * ternary
+ */
+public class JavaApp4 {
+    public static void main(String argc[]){
+        int aNum1, aNum2, aNum3;
+        aNum1 = 5; aNum2 = 5; aNum3 = 1;
+        int theRezult = aNum2 == 0 ? 0 : aNum1/aNum2 + aNum3;
+        System.out.println("Result equal = " + theRezult);
+    }
+}
