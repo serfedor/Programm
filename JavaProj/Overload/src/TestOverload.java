2df7cd87b41a713960d8c910394862a1583d259e
SerLonic <sergeyfedorenko1997@gmail.com>
2017-02-08 20:58:52 +0200

    fix bugs

--- JavaProj/Overload/src/TestOverload.java
+++ JavaProj/Overload/src/TestOverload.java
@@ -2,8 +2,8 @@
  * Created by SerLonic on 07.02.2017.
  */
 public class TestOverload {
-    String name;
-    String phoneNumber;
+    private String name;
+    private String phoneNumber;
     TestOverload(String aName, String aphoneNumber) {
         name = aName;
         phoneNumber = aphoneNumber;
@@ -13,12 +13,14 @@ public class TestOverload {
         phoneNumber = object.phoneNumber;
     }
     boolean equalPhoneNumber(TestOverload object){
-        return (name == object.name) && (phoneNumber == object.phoneNumber);
+        return name.equals(object.name) && phoneNumber.equals(object.phoneNumber);
     }
-    String setPhoneNumber(String aPhoneNumber){
-       return phoneNumber = aPhoneNumber;
+    void setPhoneNumber(String aPhoneNumber){
+       phoneNumber = aPhoneNumber;
     }
     String getPhoneNumber(){
         return phoneNumber;
     }
+    void setName(String aName){name = aName;}
+    String getName(){return name;}
 }
