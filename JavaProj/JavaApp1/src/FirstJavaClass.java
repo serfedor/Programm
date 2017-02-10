68450ee60f86cb6299191d38454ec8d3659d6648
SerLonic <sergeyfedorenko1997@gmail.com>
2017-02-10 13:30:19 +0200

    delete projects

--- JavaProj/JavaApp1/src/FirstJavaClass.java
+++
@@ -1,9 +0,0 @@
-/*
-Created by SerLonic on 30.01.2017.
-My first java code.
- */
-public class FirstJavaClass {
-    public static void main(String argc[]) {
-        System.out.println("Hello Java language!");
-    }
-}
